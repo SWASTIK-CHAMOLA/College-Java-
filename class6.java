3d358d145ac448cb1de53da971264fdf5e58d306
SWASTIK CHAMOLA <swastik.3814031@gmail.com>
2026-02-12 08:41:20 +0530

    Add files via upload

---
+++ class6.java
@@ -0,0 +1,14 @@
+package SAMPLE_PACKAGE;
+class Car{
+    int n;
+    int mileage;
+    Car(int price,int mileage){
+        this.n = price;
+        this.mileage = mileage;
+    }
+}
+
+public class class6 {
+    public static void main(String[] args) {
+}
+}
---
+++ class6_2.java
@@ -0,0 +1,10 @@
+package SAMPLE_PACKAGE;
+
+import SAMPLE_PACKAGE.Car;
+public class class6_2 {
+    public static void main(String[] args) {
+        Car myCar = new Car(25000, 30);
+        System.out.println("Car Price: " + myCar.n);
+        System.out.println("Car Mileage: " + myCar.mileage);
+    }
+}
